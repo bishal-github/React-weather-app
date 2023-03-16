62e70304517da2a844c37356b82e2b014032ca16
bishal-github <shreesbishal44@gmail.com>
2023-03-16 17:31:56 +0545

    adding conent

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,34 @@ import axios from 'axios'
 
 function App() {
 
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=Lalitpur,NPL&appid=3560d73d48aae239fd5f3e6ace085a23`;
+
   return (
-    <div className="App">
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Kathmandu</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="descriptions">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65°F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            12 MPH
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
